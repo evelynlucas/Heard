7a775ae88c518b53298d1becd23e171e23b2dbf1
Akash <akash@tanjilas-Air.fios-router.home>
2020-07-25 20:05:08 -0400

    data binding and navigation gradle

--- app/src/main/java/org/pursuit/heard/MainActivity.java
+++ app/src/main/java/org/pursuit/heard/MainActivity.java
@@ -1,6 +1,6 @@
 package org.pursuit.heard;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import org.pursuit.heard.mainFragments.AddArtistFragment;
--- app/src/main/java/org/pursuit/heard/mainFragments/LoginFragment.java
+++ app/src/main/java/org/pursuit/heard/mainFragments/LoginFragment.java
@@ -2,13 +2,12 @@ package org.pursuit.heard.mainFragments;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -21,7 +20,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import org.pursuit.heard.R;
-import org.pursuit.heard.database.UserProfile;
 
 import static android.content.Context.MODE_PRIVATE;
 
